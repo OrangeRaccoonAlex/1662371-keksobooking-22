78d296f6808e97bfe6112438b186c1ab56c49aeb
Potapova <pava94@bk.ru>
2021-02-05 00:11:55 +0300

    fix: добавила условия для min и max

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-function getRandomInt(min, max){
-  let num =  Math.random() * (max - min) + min;
-  return Number(num.toFixed(1));
+// Функция для генерации положительного числа в т.ч. с плавающей запятой в заданном диапозоне
+
+function getRandomInt(min, max, diapason){
+  if (min < 0 || max < 0) {
+    console.log('диапазон задан не верно')
+  } else if (max < min) {
+    console.log('Максимальное значение не должно быть меньше минимального')
+  } else {
+    let wholeNumber = Math.random() * (max - min + 1) + min;
+
+    return Math.floor(wholeNumber * diapason) / diapason;
+  }
 }
-getRandomInt(0, 20);
+console.log(getRandomInt(0, 20, 100));

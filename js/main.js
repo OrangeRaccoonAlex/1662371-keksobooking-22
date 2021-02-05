6a459c00ce8864777c35b2b3e6a10695c4a1636e
Potapova <pava94@bk.ru>
2021-02-05 18:37:03 +0300

    fix: убрала консоль логи

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
 
 function getRandomInt(min, max, diapason){
   if (min < 0 || max < 0) {
-    console.log('диапазон задан не верно')
+    alert('диапазон задан не верно')
   } else if (max < min) {
-    console.log('Максимальное значение не должно быть меньше минимального')
+    alert('Максимальное значение не должно быть меньше минимального')
   } else {
     let wholeNumber = Math.random() * (max - min + 1) + min;
     // возвращаю число с заданным количеством знаков после запятой
     return Math.floor(wholeNumber * diapason) / diapason;
   }
 }
-console.log(getRandomInt(0, 20, 100));
+getRandomInt(0, 20, 100);

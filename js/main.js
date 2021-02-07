249ccdef17042982287baaac053c9648c5bbca7f
Potapova <pava94@bk.ru>
2021-02-08 00:39:12 +0300

    fix: сделала две функции для получения целого числа и числа с плавающей запятой

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
 // Функция для генерации положительного числа в т.ч. с плавающей запятой в заданном диапозоне
 
-function getRandomInt(min, max, diapason){
+function getRandomInt(min, max) {
   if (min < 0 || max < 0) {
-    alert('диапазон задан не верно')
+    alert('Диапазон задан не верно')
   } else if (max < min) {
     alert('Максимальное значение не должно быть меньше минимального')
   } else {
-    let wholeNumber = Math.random() * (max - min + 1) + min;
-    // возвращаю число с заданным количеством знаков после запятой
-    return Math.floor(wholeNumber * diapason) / diapason;
+    const floatNumber = Math.random() * (max - min + 1) + min;
+    const intNumber = Math.floor(floatNumber);
+    return intNumber;
   }
 }
-getRandomInt(0, 20, 100);
+
+getRandomInt(0, 20);
+
+function getRandomFloat(min, max, fractionDigits) {
+  if (min < 0 || max < 0) {
+    alert('Диапазон задан не верно')
+  } else if (max < min) {
+    alert('Максимальное значение не должно быть меньше минимального')
+  } else {
+    const power = Math.pow(10, fractionDigits);
+    let floatNumber = Math.random() * (max - min + 1) + min;
+    floatNumber = Math.floor(floatNumber * power) / power;
+    return floatNumber;
+  }
+}
+getRandomFloat(0, 20, 3);
+
